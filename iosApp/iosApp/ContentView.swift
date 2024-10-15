736e43e00f6c6745a53070d4ca50943464ea90c6
Timas Ljungdahl <timmitommi@Timass-MacBook-Pro.local>
2024-10-15 16:00:59 +0200

    Update grade and build iOS app

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,6 +3,8 @@ import Shared
 
 struct ContentView: View {
     @State private var showContent = false
+    @State private var movies: [ApiMovie] = []
+    
     var body: some View {
         VStack {
             Button("Click me!") {
@@ -11,12 +13,20 @@ struct ContentView: View {
                 }
             }
 
+            Button("First movie") {
+                Task {
+                    movies = try await Greeting().getPopularMovies().results
+                }
+            }
+            
+            Text(movies.first?.title ?? "Loading...")
+            
             if showContent {
                 VStack(spacing: 16) {
                     Image(systemName: "swift")
                         .font(.system(size: 200))
                         .foregroundColor(.accentColor)
-                    Text("SwiftUI: \(Greeting().greet())")
+                    Text("SwiftUI")
                 }
                 .transition(.move(edge: .top).combined(with: .opacity))
             }
